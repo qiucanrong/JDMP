1adf0a274ded6719f7438563ecd8d410a0849ac4
qiucanrong <qiucanrong0124@outlook.com>
2025-10-10 16:07:02 -0400

    category 4 debug attempt 2

--- jdmp_v1.py
+++ jdmp_v1.py
@@ -107,7 +107,7 @@ if desc_file:
     #st.session_state["desc_end_date_col"] = desc_end_date_col
 
 # --- template file handling ---
-if template_file:
+if template_df is not None:
     # select copyright info
     template_rights_type = st.selectbox("Select Copyright Information", [None, "STANDARD", "OTHER"])
     if template_rights_type == "STANDARD":
